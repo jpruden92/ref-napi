89cbbef263f72ea14ef60729ed60cb010127fda1
Jorge Prudencio <jorge.prudencio@sngular.team>
2020-01-29 00:00:51 +0800

    Add build

---
+++ build/Release/.deps/Release/obj.target/binding/src/binding.o.d
@@ -0,0 +1,13 @@
+cmd_Release/obj.target/binding/src/binding.o := g++ '-DNODE_GYP_MODULE_NAME=binding' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/opt/app-root/src/.node-gyp/10.16.3/include/node -I/opt/app-root/src/.node-gyp/10.16.3/src -I/opt/app-root/src/.node-gyp/10.16.3/deps/openssl/config -I/opt/app-root/src/.node-gyp/10.16.3/deps/openssl/openssl/include -I/opt/app-root/src/.node-gyp/10.16.3/deps/uv/include -I/opt/app-root/src/.node-gyp/10.16.3/deps/zlib -I/opt/app-root/src/.node-gyp/10.16.3/deps/v8/include -I/opt/app-root/src/node_modules/ref-napi/node_modules/node-addon-api  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/binding/src/binding.o.d.raw   -c -o Release/obj.target/binding/src/binding.o ../src/binding.cc
+Release/obj.target/binding/src/binding.o: ../src/binding.cc \
+ /opt/app-root/src/node_modules/ref-napi/node_modules/node-addon-api/napi.h \
+ /opt/app-root/src/.node-gyp/10.16.3/include/node/node_api.h \
+ /opt/app-root/src/.node-gyp/10.16.3/include/node/node_api_types.h \
+ /opt/app-root/src/node_modules/ref-napi/node_modules/node-addon-api/napi-inl.h \
+ /opt/app-root/src/node_modules/ref-napi/node_modules/node-addon-api/napi-inl.deprecated.h
+../src/binding.cc:
+/opt/app-root/src/node_modules/ref-napi/node_modules/node-addon-api/napi.h:
+/opt/app-root/src/.node-gyp/10.16.3/include/node/node_api.h:
+/opt/app-root/src/.node-gyp/10.16.3/include/node/node_api_types.h:
+/opt/app-root/src/node_modules/ref-napi/node_modules/node-addon-api/napi-inl.h:
+/opt/app-root/src/node_modules/ref-napi/node_modules/node-addon-api/napi-inl.deprecated.h:
